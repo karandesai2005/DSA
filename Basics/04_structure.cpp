a7ffaa6d809710003cd42857f95d3db843b6f79f
karandesai2005 <karanishudesai2@gmail.com>
2025-07-08 19:37:06 +0530

    basics -2

--- Basics/04_structure.cpp
+++ Basics/04_structure.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 struct Rectangle
 {
-    int length;
-    int breadth;
+    int length;//4
+    int breadth;//4
+    char x;//1
 };
 int main()
 {   
@@ -13,6 +14,7 @@ int main()
     r.length = 12;
     r.breadth = 14;
     cout << r.length << endl;
-
+    cout << sizeof(r);// it would be 12 bytes instead of 9 bytes because its easy for computer to read 4 bytes 
+    // so it will tak 4 bytes for char but would use only 1 byte
     return 0;
 }
